37bd9acdc0ef1d8d2a0d2b94dc9a17d157d9e437
Roni Shabo <roni.shabo@gmail.com>
2019-02-26 18:34:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array[-2] << " and "
     array.join
   elsif array.length >= 3
-    array[-1] = "and #{array[-1}"
+    array[-1].prepend = "and #{array[-1}"
     return array.join(", ")
   end
 end
