c2a29a4cbe8b709be5e6d3b7e2bbaaff6c1080fe
Roni Shabo <roni.shabo@gmail.com>
2019-02-26 18:21:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 
-    
-  elsif  
-    array.length == 2
-    array.join(" and ")
+    return
+  elsif array.length == 2
+    return array.join(" and ")
   else
     array.join
   end
